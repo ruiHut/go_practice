fdc5d0af34d852d14f2b8d5081bd1c52ef664cf0
mac_for_hut <1147681296@qq.com>
2019-03-05 12:07:36 +0800

    新增 go 实例 字符串反转

--- src/Test.go
+++ src/Test.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+//  	hello world
+
 func main() {
 	fmt.Println("hello, world!")
 }
---
+++ src/reverse_go.go
@@ -0,0 +1,19 @@
+package main
+
+// 将字符串反转
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print(Reverse("hello"))
+}
+
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
