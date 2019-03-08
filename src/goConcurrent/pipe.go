00707ba1b2e7904ed9e0b95b0e8781237100ed4b
mac_for_hut <1147681296@qq.com>
2019-03-08 15:55:32 +0800

    pipi 管道修改

--- src/goConcurrent/pipe.go
+++ src/goConcurrent/pipe.go
@@ -1,10 +1,32 @@
 package main
 
+// 管道 只能运行一次
+
 import (
+	"fmt"
 	"os/exec"
 )
 
 func main() {
 	cmdo := exec.Command("echo", "-n", "my first command come from golong,")
-	cmdo.Start()
+	//if err:= cmdo.Start(); err != nil {
+	//	fmt.Println("the command cant startup ", err)
+	//	return
+	//}
+
+	// 创建一个能获取此命令都输出管道
+	stdouto, err := cmdo.StdoutPipe() // StdoutPipe（） 方法表示将管道的输出值赋值给 变量stdouto 类型是 io.readcloser
+	if err != nil {
+		fmt.Println("cant start is ")
+		return
+	}
+
+	// 获取输出
+	outputO := make([]byte, 30)
+
+	if n, err := stdouto.Read(outputO); err == nil {
+		fmt.Println("result is  %s :", outputO[:n])
+	} else {
+		fmt.Println("cant not open it")
+	}
 }
