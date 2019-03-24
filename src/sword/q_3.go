3a6f37799502a4e58608deed6d3918a7e32ce040
mac_for_hut <1147681296@qq.com>
2019-03-24 22:04:54 +0800

    add q_3 solution bu hash

--- src/sword/q_3.go
+++ src/sword/q_3.go
@@ -1,7 +1,10 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	testArray := []int32{1, 2, 2}
+	fmt.Println(DuplicateNumberByHashMap(testArray))
 }
 
 // 找出数组中重复的数组 o（n） 空间o（n）
@@ -17,5 +20,19 @@ func DuplicateNumber(array []int32) int32 {
 		}
 	}
 
+	return -1
+}
+
+// by hash
+func DuplicateNumberByHashMap(array []int32) int32 {
+	var res2 = map[int32]int{}
+	for _, vars := range array {
+		i := res2[vars]
+		if i == 0 {
+			res2[vars] = 1
+		} else {
+			return vars
+		}
+	}
 	return -1
 }
